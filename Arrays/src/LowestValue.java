ece313c29d147b29f1ac7c450c36ec960b9d8396
naingminkhant <tharkhant777@gmail.com>
2025-01-07 22:32:18 +0630

    finding lowest value - simple algo

---
+++ Arrays/src/LowestValue.java
@@ -0,0 +1,18 @@
+// Simple Algorithm for finding lowest value (using loop through all data)
+// Time Complexity O(n)
+public class LowestValue {
+    public static void main(String[] args) {
+        int[] arr = {7, 12, 9, 4, 11, 2};
+        System.out.println("Lowest Value is : " + findLowest(arr));
+    }
+
+    private static int findLowest(int[] arr) {
+        int lowest = arr[0];
+
+        for (int i : arr) {
+            if (i < lowest) lowest = i;
+            else continue;
+        }
+        return lowest;
+    }
+}
