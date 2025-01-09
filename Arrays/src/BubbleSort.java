82043b706cae256dafd2f467c321fe6391cb069b
naingminkhant <naingminkhant@myansis.com>
2025-01-09 14:05:47 +0630

    bubble sort update

--- Arrays/src/BubbleSort.java
+++ Arrays/src/BubbleSort.java
@@ -5,17 +5,26 @@ public class BubbleSort {
     public static void main(String[] args) {
         int[] arr = {7, 12, 9, 11, 3};
 
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
-                if (arr[j] > arr[i]) {
-                    System.out.println("Swap :" + arr[i] + " and " + arr[j]);
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+        System.out.println("Initial Array : " + Arrays.toString(arr) + "\n");
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            boolean swap = false;
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    System.out.println("Swap :" + arr[j] + " and " + arr[j + 1]);
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
+                swap = true;
+                System.out.println("State (Inner Loop) " + (j + 1) + " : " + Arrays.toString(arr) + "\n");
+
             }
+            System.out.println("State (Outer Loop) " + (i + 1) + " : " + Arrays.toString(arr) + "\n");
+
+            if (!swap) break;
         }
 
-        System.out.println(Arrays.toString(arr));
+        System.out.println("Sorted Array : " + Arrays.toString(arr));
     }
 }
---
+++ Arrays/src/SelectionSort.java
@@ -0,0 +1,6 @@
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 5, 22, 11, 90, 12};
+        
+    }
+}
