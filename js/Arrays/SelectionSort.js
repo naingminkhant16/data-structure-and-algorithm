45dd0fe9b2ad2108c223308bb9948076fe80f610
naingminkhant <naingminkhant@myansis.com>
2025-01-15 15:56:52 +0630

    optimize code

--- js/Arrays/SelectionSort.js
+++ js/Arrays/SelectionSort.js
@@ -2,13 +2,18 @@ const arr = [42, 23, 74, 11, 65, 88, 19, 56, 93, 7];
 // after first loop 3,12,9,11,7
 
 for (let i = 0; i < arr.length - 1; i++) {
+    let minIndex = i;
     for (let j = i + 1; j < arr.length; j++) {
-        if (arr[j] < arr[i]) {
-            // swap
-            const temp = arr[j];
-            arr[j] = arr[i];
-            arr[i] = temp;
+        if (arr[j] < arr[minIndex]) {
+            minIndex = j;
         }
+    }
+
+    if (minIndex !== i) {
+        // swap
+        const temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
     }
 }
 
