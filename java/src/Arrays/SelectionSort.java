60b529b53fbcaef8c37c869d274fd4ca3614bbfe
naingminkhant <naingminkhant@myansis.com>
2025-01-15 16:28:56 +0630

    Selection Sort in Java

--- java/src/Arrays/SelectionSort.java
+++ java/src/Arrays/SelectionSort.java
@@ -1,8 +1,28 @@
 package Arrays;
 
+import java.util.Arrays;
+
+// Big O notation Time Complexity - O(n^2)
 public class SelectionSort {
     public static void main(String[] args) {
         int[] arr = {64, 34, 25, 5, 22, 11, 90, 12};
 
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            
+            // only swap if minIndex is changed
+            if (minIndex != i) {
+                // swap
+                int temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+            }
+        }
+        System.out.println("Selection Sorted Array : " + Arrays.toString(arr));
     }
 }
