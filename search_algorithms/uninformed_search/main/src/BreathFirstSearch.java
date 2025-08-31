321b8ebb666ac74b0a275322f96dcee8bb9804e2
naingminkhant <tharkhant777@gmail.com>
2025-08-31 22:54:44 +0630

    feat: Breath First Search for Tree - Java

---
+++ search_algorithms/uninformed_search/main/src/BreathFirstSearch.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class Node {
+    public int data;
+    public List<Node> children;
+
+    public Node(int data) {
+        this.data = data;
+        this.children = new ArrayList<>();
+    }
+}
+
+public class BreathFirstSearch {
+    private static void bsfTree(Node root) {
+        if (root == null) return;
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            // remove front
+            Node current = queue.poll();
+            System.out.println(current.data);
+            for (Node child : current.children) {
+                queue.offer(child);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        /*
+                  1
+               /     \
+              2       3
+            / | \   / | \
+           4  5  6 7  8  9
+        */
+        Node rootNode = new Node(1);
+
+        Node secondNode = new Node(2);
+        secondNode.children.add(new Node(4));
+        secondNode.children.add(new Node(5));
+        secondNode.children.add(new Node(6));
+
+        Node thirdNode = new Node(3);
+        thirdNode.children.add(new Node(7));
+        thirdNode.children.add(new Node(8));
+        thirdNode.children.add(new Node(9));
+
+        rootNode.children.add(secondNode);
+        rootNode.children.add(thirdNode);
+
+        bsfTree(rootNode);
+    }
+}
+
+
